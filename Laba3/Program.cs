1039cc7be29ef56aca51f6b8013db9fc50319252
TranzysThor <maxim.akulevich@gmail.com>
2020-09-27 23:45:09 +0300

    feat: add class object creation

--- Laba3/Program.cs
+++ Laba3/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Laba3
 {
     public class Vector
     {
-        public int NumberOfElements
+        private string ErrorCode;
+        public byte NumberOfElements
         {
             set
             {
@@ -20,11 +20,9 @@ namespace Laba3
             }
             get { return NumberOfElements; }
         }
-
         public int[] Array;
-        public string ErrorCode;
-        
-        public Vector()
+
+        /*public Vector()
         {
             Random rnd = new Random();
             for (var i = 0; i < NumberOfElements; i++)
@@ -45,7 +43,7 @@ namespace Laba3
         public Vector(int[] arr)
         {
             Array = arr;
-        }
+        }*/
         
         public int Add(int[] arr, int index, int num)
         {
@@ -60,7 +58,30 @@ namespace Laba3
     {
         static void Main(string[] args)
         {
-            
+            Vector[] vectors = new Vector[5];
+            Random rnd = new Random();
+            int[][] arr = new int[5][];
+            for (var i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new int[10];
+                for (var j = 0; j < arr[i].Length; j++)
+                {
+                    arr[i][j] = rnd.Next(0, 100);
+                }
+                vectors[i].Array = arr[i];
+                vectors[i].NumberOfElements = Convert.ToByte(arr[i].Length);
+            }
+
+            foreach (var vec in vectors)
+            {
+                for (var i = 0; i < vec.Array.Length; i++)
+                {
+                    if (vec.Array[i] == 0)
+                    {
+                        Console.WriteLine($"{vec.Array}\n");
+                    }
+                }
+            }
         }
     }
 }
