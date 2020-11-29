5723b911d342b08b7bed1728f2e5bf77197f2ab5
TranzysThor <maxim.akulevich@gmail.com>
2020-11-29 14:19:58 +0300

    docs: finish Laba10

---
+++ Laba10/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Laba10
+{
+    public class Car
+    {
+        private string _manufacturer;
+        public string Manufacturer
+        {
+            get => _manufacturer;
+            set => _manufacturer = value;
+        }
+        private IList<string> _models;
+        public IList<string> Models
+        {
+            get => _models;
+            set => _models = value;
+        }
+        public Car(string manufacturer, IList<string> models)
+        {
+            Manufacturer = manufacturer;
+            Models = models;
+        }
+        public void Del(string model)
+        {
+            if (Models.Contains(model))
+            {
+                Models.Remove(model);
+            }
+        }
+        public void Add(string model)
+        {
+            if (Models.Contains(model) == false)
+            {
+                Models.Add(model);
+            }
+            else
+            {
+                Console.WriteLine("Such model is already on the list");
+            }
+        }
+        public void LookUp(string model)
+        {
+            if (Models.Contains(model))
+            {
+                Console.WriteLine($"{Manufacturer}'s model list contains {model}");
+            }
+            else
+            {
+                Console.WriteLine($"There is no such model as {model} in {Manufacturer}'s model list");
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{Manufacturer}'s model list consists of:");
+            foreach (var model in Models)
+            {
+                Console.WriteLine(model);
+            }
+        }
+    }
+}
---
+++ Laba10/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Laba10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> WagModels = new List<string>{"Polo", "Golf", "Golf R", "Passat", "Scirocco"};
+            List<string> AudiModels = new List<string>{"TT", "R8", "Q8", "RS6", "A7"};
+            List<string> FordModels = new List<string>{"Mustang", "Mondeo", "Focus", "Focus ST", "F150", "Fusion"};
+            var Volkswagen = new Car("Volkswagen", WagModels);
+            var Audi = new Car("Audi", AudiModels);
+            var Ford = new Car("Ford", FordModels);
+            var Cars = new ObservableCollection<Car>();
+            Cars.CollectionChanged += Notify;
+            Cars.Add(Volkswagen);
+            Cars.Add(Audi);
+            Cars.Add(Ford);
+            Cars[0].Add("Touareg");
+            Cars[1].LookUp("R8");
+            Cars[2].Del("F150");
+            Cars[2].LookUp("TT");
+            Cars[0].Print();
+            Cars[2].Print();
+            Cars.Remove(Ford);
+            Console.WriteLine("\n-----------------------------------------------------------------\n");
+            Dictionary<object, object> randomDictionary = new Dictionary<object, object>();
+            randomDictionary.Add("Minsk", 2_000_000);
+            randomDictionary.Add("Maxim", 'M');
+            randomDictionary.Add(2000, "TMMLP");
+            foreach (var keyValue in randomDictionary)
+            {
+                Console.WriteLine($"{keyValue.Key} is a key to {keyValue.Value}");
+            }
+            randomDictionary.TryAdd(2020, true);
+            randomDictionary.Remove("Minsk");
+            randomDictionary.TryAdd(Audi, AudiModels);
+            Console.WriteLine("\n-----------------------------------------------------------------\n");
+            SortedList<object, object> randSortedList = new SortedList<object, object>();
+            foreach (var keyValue in randomDictionary)
+            {
+                randSortedList.Add(keyValue.Key.ToString(), keyValue.Value.ToString());
+            }
+            foreach (var keyValue in randSortedList)
+            {
+                Console.WriteLine($"{keyValue.Key} is a key to {keyValue.Value}");
+            }
+            Console.WriteLine(randSortedList.ContainsValue("TMMLP"));
+            
+        }
+        private static void Notify(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                {
+                    Car newCar = e.NewItems[0] as Car;
+                    Console.WriteLine($"Added New Manufacturer {newCar.Manufacturer}");
+                    break;
+                }
+                case NotifyCollectionChangedAction.Remove:
+                {
+                    Car oldCar = e.OldItems[0] as Car;
+                    Console.WriteLine($"Removed Manufacturer {oldCar.Manufacturer}");
+                    break;
+                }
+            }
+        }
+    }
+}
